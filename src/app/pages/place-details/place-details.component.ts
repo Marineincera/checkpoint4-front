c9fc1e28e72cb1b082c3b5a089973bca554d7583
Marine Incera <marine.incera@gmail.com>
2020-01-30 17:41:04 +0100

    update place

--- src/app/pages/place-details/place-details.component.ts
+++ src/app/pages/place-details/place-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Place } from '../../shared/models/place';
 import { PlaceService } from '../../shared/services/place.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-place-details',
@@ -12,10 +13,20 @@ export class PlaceDetailsComponent implements OnInit {
 
   placeReceived: any;
   placeToDisplay: Place;
+  representations = false;
+  editionOpen = false;
+  placeToUpdate: Place;
+
+  placeUpdateForm = this.fb.group({
+    city: [''],
+    begin: [''],
+    end: [''],
+  });
 
   constructor(private router: Router,
     private route: ActivatedRoute,
-    private placeService: PlaceService) { }
+    private placeService: PlaceService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -25,4 +36,23 @@ export class PlaceDetailsComponent implements OnInit {
     });
   }
 
+  showRepresentations() {
+    this.representations = !this.representations;
+  }
+
+  openEditingForm() {
+    this.editionOpen = !this.editionOpen;
+  }
+
+  updatePlace(id) {
+    this.placeToUpdate = {
+      city: this.placeUpdateForm.value.city || this.placeToDisplay.city,
+      begin: this.placeUpdateForm.value.begin || this.placeToDisplay.begin,
+      end: this.placeUpdateForm.value.end || this.placeToDisplay.end
+    };
+    this.placeService.update(id, this.placeToUpdate).subscribe((data) => {
+      console.log(id);
+
+    });
+  }
 }
--- src/app/shared/models/place.ts
+++ src/app/shared/models/place.ts
@@ -1,11 +1,11 @@
 import { Representation } from './representation';
 
 export class Place {
-    id: number;
+    id?: number;
     city: string;
-    begin: string;
-    end: string;
-    representations: Representation[];
+    begin?: string;
+    end?: string;
+    representations?: Representation[];
 
     constructor(place: Place) {
         Object.assign(place, this);
